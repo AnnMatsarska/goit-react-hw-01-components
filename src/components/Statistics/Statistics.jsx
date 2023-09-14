0aed9c25020dce07b139fdec8b19e64ca546c07b
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2023-09-14 21:11:44 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,10 +1,10 @@
 // import { getRandomColor } from './getRandomColor';
 import css from './Statistics.module.css';
 
-export const Statistics = ({ tittle = 'Upload stats', stats }) => {
+export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{tittle}</h2>
+      {title && <h2 className={css.title}>{title}</h2>}
 
       <ul className={css['stat-list']}>
         {stats.map(({ id, label, percentage }) => (
